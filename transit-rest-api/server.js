0aeceea9dbd0e5ef6a0dd553365ca9f6cfb8cb56
mare11 <44523703+mare11@users.noreply.github.com>
2020-06-06 14:11:17 +0200

    revert changes for server database

--- transit-rest-api/server.js
+++ transit-rest-api/server.js
@@ -3,9 +3,6 @@ const app = express();
 const port = process.env.port || 3000;
 const morgan = require('morgan');
 const controller = require('./controller');
-const sequelize = require('./sequelize');
-
-const Line = require("./model/Line.js");
 
 app.use(morgan('dev'));
 
@@ -19,28 +16,7 @@ app.use((req, res, next) => {
     });
 });
 
-sequelize.authenticate()
-    .then(() => {
-        console.log('DB connection has been established successfully.');
-        sequelize.sync({ force: true }).then(() => {
-            console.log('Drop and re-sync DB successful!');
-            app.listen(port, () => {
-                console.log(`App listening on port ${port}`);
-                console.log('Press Ctrl+C to quit.');
-
-
-                const line1 = Line.build({ name: '1', title: 'KLISA - LIMAN 1', type: 'CITY' });
-                console.log(line1 instanceof Line); // true
-                console.log(line1.title); // 'KLISA - LIMAN 1'
-
-                line1.save().then(
-                    () => {
-                        console.log('line1 was saved to the database!');
-                    });
-
-            });
-        });
-    })
-    .catch(error => {
-        console.error('Unable to connect to the DB. Error: ', error);
-    });
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    console.log('Press Ctrl+C to quit.');
+});
